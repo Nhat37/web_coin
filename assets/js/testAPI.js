00422e4f29980a75b5537c5bc2ed8b224e607c1d
Nhat37 <duong2le1@gmail.com>
2025-03-02 00:04:07 +0700

    thêm đc nvu, nhg khi lọc thì reset

--- assets/js/testAPI.js
+++ assets/js/testAPI.js
@@ -1,36 +1,18 @@
-var todosAPI = "https://dummyjson.com/todos"
-
+const todosAPI = "https://dummyjson.com/todos"
+const newTaskList = document.querySelector('.task__newList')
+const processingTaskList = document.querySelector('.task__processingList')
+        
 fetch(todosAPI)
     .then((response)=>{
         return response.json()
     })
-    .then((todos)=>{
-        const fakeTask = [{
-            "id": 132,
-            "todo": "Do something nice for someone you care about",
-            "completed": "0:3:00",
-            "userId": 152
-          },
-          {
-            "id": 21,
-            "todo": "Memorize a poem",
-            "completed": "3:3:05",
-            "userId": 133
-          },
-          {
-            "id": 176,
-            "todo": "Watch a classic movie",
-            "completed": "0:0:50",
-            "userId": 68
-          },
-          {
-            "id": 400,
-            "todo": "Watch a documentary",
-            "completed": "1:3:00",
-            "userId": 84
-          }]
-        const newTaskList = document.querySelector('.task__newList')
-        const processingTaskList = document.querySelector('.task__processingList')
+    .then(todos=>{
+        const fakeTask = [
+            { "id": 132, "todo": "Do something nice for someone you care about", "completed": "0:3:00", "userId": 152 },
+            { "id": 21, "todo": "Memorize a poem", "completed": "3:3:05", "userId": 133 },
+            { "id": 176, "todo": "Watch a classic movie", "completed": "0:0:50", "userId": 68 },
+            { "id": 400, "todo": "Watch a documentary", "completed": "1:3:00", "userId": 84 }
+        ]
         const todoList = todos["todos"]
         
         const htmlNew = fakeTask.map((task)=>{
@@ -42,7 +24,7 @@ fetch(todosAPI)
                         <div class="task__item--number">
                             <div class="task__item--coin"><span>${task.userId} </span>Coin</div>
                             <div class="task__item--time">${task.completed}</div>
-                            <button class="btn__task--submit btn">Nhận nhiệm vụ</button>
+                            <button class="btn__task--submit">Nhận nhiệm vụ</button>
                         </div>
                     </div>
                 </div>                
@@ -59,7 +41,7 @@ fetch(todosAPI)
                         <div class="task__item--number">
                             <div class="task__item--coin"><span>${todo.userId} </span>Coin</div>
                             <div class="task__item--time">${todo.completed}</div>
-                            <button class="btn__task--submit btn">${todo.completed ? "Nhận thưởng" : "Quá hạn"}</button>
+                            <button class="btn__task--submit">${todo.completed ? "Nhận thưởng" : "Quá hạn"}</button>
                         </div>
                     </div>
                 </div>                
@@ -71,3 +53,22 @@ fetch(todosAPI)
     .catch((error)=>{
         console.log(error)
     })
+
+// Lắng nghe sự kiện click vào danh sách nhiệm vụ mới
+newTaskList.addEventListener("click", function (event) {
+    if (event.target.classList.contains("btn__task--submit")) {
+        const taskItem = event.target.closest(".task__item.new");
+
+        if (taskItem) {
+            event.target.innerText = "Đang thực hiện";
+
+            // Cập nhật class để phân biệt
+            taskItem.classList.remove("new");
+            taskItem.classList.add("processing");
+
+            // Thêm vào danh sách nhiệm vụ đang thực hiện
+            processingTaskList.prepend(taskItem);
+        }
+    }
+});
+    
