2727302a263d4a9b5cbe2aec0c4e6fd6b12ab870
Nhat37 <duong2le1@gmail.com>
2025-03-02 18:21:46 +0700

    sửa logic đổi btn

--- assets/js/testAPI.js
+++ assets/js/testAPI.js
@@ -19,13 +19,16 @@ fetch(todosAPI)
             return `<div class="col l-12 m-12 c-12">                                        
                 <div class="task__item new">
                     <div class="task__item--title">Nhiệm vụ ${task.id}:</div>
-                    <div class="task__item--info f-between">
+                    <div class="task__item--body f-between">
                         <div class="task__item--text">${task.todo}</div>
                         <div class="task__item--number">
                             <div class="task__item--coin"><span>${task.userId} </span>Coin</div>
                             <div class="task__item--time">${task.completed}</div>
-                            <button class="btn__task--submit">Nhận nhiệm vụ</button>
                         </div>
+                    </div>
+                    <div class="task__item--footer">
+                        <button class="btn__task--submit">Nhận nhiệm vụ</button>
+                        <button class="btn__task--delete btn__danger hidden">Xóa</button>
                     </div>
                 </div>                
             </div>`
@@ -36,13 +39,16 @@ fetch(todosAPI)
             return `<div class="col l-12 m-12 c-12">                                        
                 <div class="task__item ${todo.completed ? 'success' : 'over'}">
                     <div class="task__item--title">Nhiệm vụ ${todo.id}:</div>
-                    <div class="task__item--info f-between">
+                    <div class="task__item--body f-between">
                         <div class="task__item--text">${todo.todo}</div>
                         <div class="task__item--number">
                             <div class="task__item--coin"><span>${todo.userId} </span>Coin</div>
                             <div class="task__item--time">${todo.completed}</div>
-                            <button class="btn__task--submit">${todo.completed ? "Nhận thưởng" : "Quá hạn"}</button>
                         </div>
+                    </div>
+                    <div class="task__item--footer">
+                        <button class="btn__task--submit" data-id="${todo.id}">${todo.completed ? "Nhận thưởng" : "Quá hạn"}</button>
+                        <button class="btn__task--delete btn__danger" data-id="${todo.id}">Xóa</button>
                     </div>
                 </div>                
             </div>`
@@ -60,11 +66,12 @@ newTaskList.addEventListener("click", function (event) {
         const taskItem = event.target.closest(".task__item.new");
 
         if (taskItem) {
-            event.target.innerText = "Đang thực hiện";
+            event.target.innerText = "Xác nhận hoàn thành";
 
             // Cập nhật class để phân biệt
             taskItem.classList.remove("new");
             taskItem.classList.add("processing");
+            taskItem.querySelector('.btn__task--delete').classList.remove('hidden')
 
             // Thêm vào danh sách nhiệm vụ đang thực hiện
             processingTaskList.prepend(taskItem);
